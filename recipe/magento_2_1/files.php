11335ca888b246ce33fade62a9ee648e84b3b019
Marcel Hauri <marcel@hauri.me>
2018-04-25 14:43:38 +0200

    apply coding standards

--- recipe/magento_2_1/files.php
+++ recipe/magento_2_1/files.php
@@ -12,9 +12,11 @@ set('static_deploy_options', '--exclude-theme=Magento/blank');
 
 task('files:compile', '{{bin/php}} {{magento_bin}} setup:di:compile');
 task('files:static_assets', '{{bin/php}} {{magento_bin}} setup:static-content:deploy {{languages}} {{static_deploy_options}}');
-task('files:permissions',
+task(
+    'files:permissions',
     'cd {{magento_dir}} && find var vendor pub/static pub/media app/etc -type f -exec chmod g+w {} \; ' .
-    '&& find var vendor pub/static pub/media app/etc -type d -exec chmod g+w {} \; && chmod u+x bin/magento');
+    '&& find var vendor pub/static pub/media app/etc -type d -exec chmod g+w {} \; && chmod u+x bin/magento'
+);
 
 desc('Generate Magento Files');
 task('files:generate', [
