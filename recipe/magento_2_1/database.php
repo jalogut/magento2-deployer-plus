f27929bf12173e90eaed234bbfeb42bce349314f
Rik <5927342+rikwillems@users.noreply.github.com>
2019-02-06 09:42:00 +0100

    No setup upgrade but db-schema and db-data upgrade

--- recipe/magento_2_1/database.php
+++ recipe/magento_2_1/database.php
@@ -7,4 +7,7 @@
 
 namespace Deployer;
 
-task('database:upgrade', '{{bin/php}} {{magento_bin}} setup:upgrade --keep-generated');
+task('database:upgrade', function () {
+    run('{{bin/php}} {{release_path}}/{{magento_bin}} setup:db-schema:upgrade --no-interaction');
+    run('{{bin/php}} {{release_path}}/{{magento_bin}} setup:db-data:upgrade --no-interaction');
+});
