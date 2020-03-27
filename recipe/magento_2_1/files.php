aacd9b8b9745ca8e8f3b41799ab59755eb8d0c8c
Erfan <contact@erfanimani.com>
2020-03-27 13:01:35 +0800

    Removed unnecessary cd

--- recipe/magento_2_1/files.php
+++ recipe/magento_2_1/files.php
@@ -17,7 +17,7 @@ task('files:compile', '{{bin/php}} {{magento_bin}} setup:di:compile');
 task('files:static_assets', '{{bin/php}} {{magento_bin}} setup:static-content:deploy {{languages}} {{static_deploy_options}}');
 task(
     'files:permissions',
-    'cd {{magento_dir}} && chmod -R g+w var vendor pub/static pub/media app/etc && chmod u+x bin/magento'
+    'chmod -R g+w var vendor pub/static pub/media app/etc && chmod u+x bin/magento'
 );
 
 desc('Generate Magento Files');
