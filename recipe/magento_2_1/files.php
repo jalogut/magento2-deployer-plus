6a5959753052df1d52910b42e3c1ed4e83d4034f
Timon de Groot <timon@mooore.nl>
2021-12-02 16:47:08 +0100

    Run dump-autoload with explicit --no-dev

--- recipe/magento_2_1/files.php
+++ recipe/magento_2_1/files.php
@@ -14,7 +14,7 @@ set('languages', 'en_US');
 set('static_deploy_options', '--exclude-theme=Magento/blank');
 
 task('files:compile', '{{bin/php}} {{magento_bin}} setup:di:compile');
-task('files:optimize-autoloader', '{{bin/composer}} dump-autoload --optimize --apcu');
+task('files:optimize-autoloader', '{{bin/composer}} dump-autoload --no-dev --optimize --apcu');
 task('files:static_assets', '{{bin/php}} {{magento_bin}} setup:static-content:deploy {{languages}} {{static_deploy_options}}');
 task(
     'files:permissions',
